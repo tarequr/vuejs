1365a98434ea2ee4690f248969c94535e04c03d5
tarequr <tarequrr8@gmail.com>
2025-08-16 16:58:41 +0600

    chore: removed unnecessary data and use some features: conditional, loop, function

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Tarequr Rahman Sabbir",
+      status: true,
+      tasks: ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"],
+      link: "https://google.com",
+    };
+  },
+
+  methods: {
+    toggleStatus() {
+      // this.status = !this.status;
+      if (this.status) {
+        this.status = false;
+      } else {
+        this.status = true;
+      }
+      // this.status = this.status ? false : true;
+      // this.status = this.status === true ? false : true;
+      // this.status = !this.status;
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1>{{ name }}</h1>
+    <p v-if="status">Status: Active</p>
+    <p v-else>Status: Inactive</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <h3>Tasks</h3>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+    </ul>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <a :href="link" target="_blank">Go to Google</a>
+
+    <p v-if="tasks.length > 0">You have {{ tasks.length }} tasks.</p>
+    <p v-else>You have no tasks.</p>
+
+    <!-- <button v-on:click="status = !status">Change Status</button> -->
+    <button v-on:click="toggleStatus">Change Status</button>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
