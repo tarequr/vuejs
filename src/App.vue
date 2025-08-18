5ce947fad8ad2819544b39d6342651c0c5be7f58
tarequr <tarequrr8@gmail.com>
2025-08-18 23:02:47 +0600

    chore: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+</script>
+
 <template>
-  <h1>Vue Jobs</h1>
-  <h1 class="text-3xl font-bold underline bg-green-500">Hello world!</h1>
+  <Navbar />
 </template>
