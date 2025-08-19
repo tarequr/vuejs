301a6c65917c555dd6cc4157f644fcb771a1ad11
tarequr <tarequrr8@gmail.com>
2025-08-19 23:08:47 +0600

    chore: add hero section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
+import Hero from "./components/Hero.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
+  <Hero
+    title="Vue Jobs for Vue Devs"
+    subtitle="Find the Vue job that fits your skills and needs."
+  />
 </template>
