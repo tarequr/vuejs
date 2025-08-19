07a5afb3528a7fe14c18508a7c0249a28dc523a2
tarequr <tarequrr8@gmail.com>
2025-08-19 23:24:04 +0600

    chore: added card section

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { defineProps } from "vue";
+import Card from "./Card.vue";
 </script>
 
 <template>
   <section class="py-4">
     <div class="container-xl lg:container m-auto">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-        <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+        <Card>
           <h2 class="text-2xl font-bold">For Developers</h2>
           <p class="mt-2 mb-4">
             Browse our Vue jobs and start your career today
@@ -17,8 +18,8 @@ import { defineProps } from "vue";
           >
             Browse Jobs
           </a>
-        </div>
-        <div class="bg-green-100 p-6 rounded-lg shadow-md">
+        </Card>
+        <Card bg="bg-green-100">
           <h2 class="text-2xl font-bold">For Employers</h2>
           <p class="mt-2 mb-4">
             List your job to find the perfect developer for the role
@@ -29,7 +30,7 @@ import { defineProps } from "vue";
           >
             Add Job
           </a>
-        </div>
+        </Card>
       </div>
     </div>
   </section>
