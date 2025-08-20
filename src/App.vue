830525c526e9382f1789376a18627b406ed9522b
tarequr <tarequrr8@gmail.com>
2025-08-20 22:07:24 +0600

    chore: create dummy file for jobs data and create component for job list

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Hero from "./components/Hero.vue";
 import HomeCards from "./components/HomeCards.vue";
+import JobListngs from "./components/JobListngs.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
@@ -8,4 +9,5 @@ import Navbar from "./components/Navbar.vue";
   <Navbar />
   <Hero />
   <HomeCards />
+  <JobListngs />
 </template>
