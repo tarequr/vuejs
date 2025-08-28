9212d9d8beb5442dbdcb79b8b230b031da01a1e8
tarequr <tarequrr8@gmail.com>
2025-08-28 23:05:31 +0600

    chore: separate home page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
-import Hero from "./components/Hero.vue";
-import HomeCards from "./components/HomeCards.vue";
-import JobListngs from "./components/JobListngs.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
-  <Hero />
-  <HomeCards />
-  <JobListngs :limit="3" />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
+import JobListngs from "./components/JobListngs.vue";
+</script>
+
+<template>
+  <Hero />
+  <HomeCards />
+  <JobListngs :limit="3" />
+</template>
