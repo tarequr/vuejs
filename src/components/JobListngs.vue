fc172d57ae6e642a3e44ea908e2a406123faa2ce
tarequr <tarequrr8@gmail.com>
2025-08-31 22:05:00 +0600

    feat: show loading

--- src/components/JobListngs.vue
+++ src/components/JobListngs.vue
@@ -4,6 +4,7 @@ import axios from "axios";
 import JobListng from "./JobListng.vue";
 import { ref, defineProps, onMounted, reactive } from "vue";
 import { RouterLink } from "vue-router";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 defineProps({
   limit: Number,
@@ -38,7 +39,14 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+
+      <!-- Show job when done loading -->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListng
           v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
